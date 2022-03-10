2fd1bc1888918ea661b69b6423adbdc2721165de
S.Brand <s.brand@dbg-gl.de>
2022-03-10 11:57:48 +0100

    noTiming variante added

--- Sources/BenchLIB/BenchLIB.swift
+++ Sources/BenchLIB/BenchLIB.swift
@@ -3,7 +3,7 @@ import Foundation
 public func benchmark_lib(){
     let t=CFAbsoluteTimeGetCurrent()
     var sum:Double=0
-    var w1:Double = 2  //flipping between 1 and 2
+    var w1:Double = 2  //flipping between 1 and 2!
     var w2:Double = 0
     var w3:Double = 0
     print("Go...\(t)")
@@ -36,4 +36,19 @@ public func benchmark_fast(){
     print("Ende: \(t2)")
     print("Zeit: +\(t2-t)")
     print(sum)
+}
+
+
+public func benchmark_fast_noTiming(){
+    var sum:Double=0
+    var w1:Double = 2  //flipping between 1 and 2
+    var w2:Double = 0
+    var w3:Double = 0
+    print("Go \(t)")
+    for i in 0...10000000 {  //10000000
+        w1 = 3-w1
+        (w2 = Double((2*i) - 1))
+        (w3 = Double((w1-2)/w2))
+        (sum += w3)
+    }
 }
