d04d6f6ac2817868bf012dc4fabd3b2ec227d79d
S.Brand <s.brand@dbg-gl.de>
2022-03-10 12:02:28 +0100

    error missing variable fixed

--- Sources/BenchLIB/BenchLIB.swift
+++ Sources/BenchLIB/BenchLIB.swift
@@ -44,7 +44,6 @@ public func benchmark_fast_noTiming(){
     var w1:Double = 2  //flipping between 1 and 2
     var w2:Double = 0
     var w3:Double = 0
-    print("Go \(t)")
     for i in 0...10000000 {  //10000000
         w1 = 3-w1
         (w2 = Double((2*i) - 1))
