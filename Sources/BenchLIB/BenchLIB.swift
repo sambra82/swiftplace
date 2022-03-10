c32b4bb590acd8002f01d06d81c39840dcdc2b3c
S.Brand <s.brand@dbg-gl.de>
2022-03-10 12:18:57 +0100

    removed foundation depency

--- Sources/BenchLIB/BenchLIB.swift
+++ Sources/BenchLIB/BenchLIB.swift
@@ -1,16 +1,3 @@
-public func benchmark_lib_noTiming(){
-    var sum:Double=0
-    var w1:Double = 2  //flipping between 1 and 2!
-    var w2:Double = 0
-    var w3:Double = 0
-    for i in 0...10000000 {  //10000000
-        w1 = powl(-1,Double(i+1))
-        w2 = Double((2*i) - 1)
-        w3 = w1/w2
-        sum += w3
-    }
-}
-
 public func benchmark_fast_noTiming(){
     var sum:Double=0
     var w1:Double = 2  //flipping between 1 and 2
