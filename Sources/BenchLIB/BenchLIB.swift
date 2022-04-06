d75e388223f8cf78f68396c15190260f15e318cf
S.Brand <s.brand@dbg-gl.de>
2022-04-06 13:26:02 +0200

    add optimised version

--- Sources/BenchLIB/BenchLIB.swift
+++ Sources/BenchLIB/BenchLIB.swift
@@ -11,6 +11,21 @@ public func benchmark_fast_noTiming(){
     }
 }
 
+public func benchmark_fast_noTiming_optimzed(){
+    var sum:Double=0
+    var w1:Double = 2  //flipping between 1 and 2
+    var w2:Double = 0
+    var w3:Double = 0
+    var i:Int = 0
+    while i<10000000 {  //10000000
+        w1 = 3-w1
+        (w2 = Double((2*i) - 1))
+        (w3 = Double((w1-2)/w2))
+        (sum += w3)
+        i+=1
+    }
+}
+
 public func anotherBench(){
     let digitCounts = Array(repeating: 0.0, count: 1000000)
     var digitCounts2 = Array(repeating: 0.0, count: 1000000)
